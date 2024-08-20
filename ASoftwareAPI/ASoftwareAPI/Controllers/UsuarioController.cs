b205b71120347e3349c78a8cf6422eedf5bda575
Austregiselo Junior <austregiselosvj@gmail.com>
2024-08-19 21:48:38 -0300

    Criando UsuarioController

---
+++ ASoftwareAPI/ASoftwareAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using ASoftwareVersaoFisioterapiaAPI.Context;
+using ASoftwareVersaoFisioterapiaAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASoftwareVersaoFisioterapiaAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly ASoftwareVersaoFisioterapiaAPIContext _context;
+
+        public UsuarioController(ASoftwareVersaoFisioterapiaAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> Get()
+        {
+            var usuario = _context.Usuarios.ToList();
+            return (usuario is null) ? NotFound("Produtos não encontrados.") : usuario;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterUsuario")]
+        public ActionResult<Usuario> GetById(int id)
+        {
+            var usuario = _context.Usuarios.FirstOrDefault(usuario => usuario.UsuarioId == id);
+            return (usuario is null) ? NotFound("Produtos não encontrados.") : usuario;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterUsuario", new { id = usuario.UsuarioId }, usuario);
+        }
+    }
+}
